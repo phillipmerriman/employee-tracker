53db59e67811787c07c68691587ff73fcd5d33f9
Phillip Merriman <phillipmerriman@gmail.com>
2020-12-23 11:21:04 -0600

    finished view querys

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const cTable = require('console.table');
 const logo = require('asciiart-logo');
 const db = require('./db/connections');
+const connection = require('./db/connections');
 
 // console.log(db);
 
@@ -15,5 +16,74 @@ function init () {
 }
 
 function loadMainPrompts () {
-    inquirer.prompt([]);
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'firstQuestion',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employess',
+                // 'View all employees by department',
+                // 'View all employees by manager',
+                'Add employee',
+                // 'Remove employee',
+                'Update employee role',
+                // 'Update employee manager',
+                'View all roles',
+                'Add role',
+                // 'Remove role',
+                'View all departments',
+                'Add department',
+                // 'Remove department',
+                // 'View utilized budget of department',
+                'Exit'
+            ]
+        }
+    ]).then((response) => {
+        console.log(response.firstQuestion);
+        switch (response.firstQuestion) {
+            case 'View all employess':
+                viewAllEmployees();
+                break;
+            case 'Add employee':
+                viewAllEmployees();
+                break;
+            case 'Update employee role':
+                viewAllEmployees();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'Add role':
+                viewAllEmployees();
+                break;
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'Add department':
+                viewAllEmployees();
+                break;
+        }
+    });
+}
+
+function viewAllEmployees () {
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+function viewAllRoles () {
+    connection.query("SELECT * FROM role", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+function viewAllDepartments () {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
 }
